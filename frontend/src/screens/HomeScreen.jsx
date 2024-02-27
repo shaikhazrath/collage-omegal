a139461734cd45cf85d1c23d4f7165974504951a
Shaik hazrathali <108566186+shaikhazrath@users.noreply.github.com>
2024-02-27 07:54:46 +0530

    Update HomeScreen.jsx

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -9,17 +9,17 @@ const HomeScreen = () => {
 <div className="flex  items-center justify-center ">
 <h1 className=" font-extrabold md:text-8xl text-6xl text-orange-500  text-center uppercase ">Campus Omegal</h1>
 </div>
-<h5 className="font-medium md:text-4xl text-sm text-gray-600 ">Video chat is comesoon...</h5>
+<h5 className="font-medium md:text-4xl text-sm text-gray-600 ">Video stream  comingsoon...</h5>
 <div>
 
 <button
-  className="text-white bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300  shadow-lg shadow-orange-500/50 font-bold rounded-md text-xl px-5 py-2.5 text-center me-2 mb-2 animate-bounce  mt-5  "
+  className="text-white bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300  shadow-lg shadow-orange-500/50 font-bold rounded-md text-xl px-5 py-2.5 text-center me-2 mb-2 mt-5  "
   onClick={() => navigate("/chatroom")}
 >
   chat
 </button>
 
-<button type="button" class="text-white bg-gray-900 font-bold rounded-md text-xl px-5 py-2.5 text-center me-2 mb-2   mt-5 " disabled>Video</button>
+  {/*<button type="button" class="text-white bg-gray-900 font-bold rounded-md text-xl px-5 py-2.5 text-center me-2 mb-2   mt-5 " disabled>Video</button>*/}
 </div>
 
 </div>
